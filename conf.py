09665884becae716d41d262cdd3c0348dd81e2fe
Maxime Vidori <mvidori@redhat.com>
2016-07-04 15:50:53 +0200

    Do not take the index from submodule
    Change-Id: I58bf19e112841459a860d9e7a09b061e283ccf80

--- conf.py
+++ conf.py
@@ -80,7 +80,10 @@ language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build', '**/README.rst']
+exclude_patterns = [
+    '_build', '**/README.rst',
+    'python-dciclient/doc/source/index.rst'
+]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
