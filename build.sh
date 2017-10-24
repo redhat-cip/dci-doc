a17d1b0c707ca8987a350f95fe992abe5ca04cf6
Yanis Guenane <yguenane@redhat.com>
2017-10-24 14:41:33 +0200

    dci-doc: use new pattern
    Change-Id: I2e8ca574ce20743fc5d6947377a450289292188b

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+projects="dci-ui"
+for project in ${projects}
+do
+    pushd /tmp
+    rm -rf ${project}
+    mkdir ${project}
+    curl -L https://api.github.com/repos/redhat-cip/${project}/tarball | tar xz --directory ${project} --strip=1
+    popd
+    rm -rf ./src/${project}
+    mkdir ./src/${project}
+    cp /tmp/${project}/README.md ./src/${project}/README.md
+    cp -r /tmp/${project}/docs/ ./src/${project}/docs/ 2>/dev/null
+    rm -rf /tmp/${project}
+done
+
+npm install
+npm run build
