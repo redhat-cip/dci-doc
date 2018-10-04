87c1659f841b8565b37c9d311fb1f85cff312cb8
Gonéri Le Bouder <goneri@redhat.com>
2018-10-04 11:22:03 -0400

    zuul: use a local Zuul project
    If we are in the Zuul context, we load the .md from the local project.
    
    Change-Id: If8e913a7c909b42ef6cd5100dc672b3fb60bd038

--- build.sh
+++ build.sh
@@ -3,17 +3,13 @@
 projects="dci-ui python-dciclient python-dciauth dci-control-server dci-ansible dci-openstack-agent dci-dev-env dci-rhel-agent"
 for project in ${projects}
 do
-    pushd /tmp
-    rm -rf ${project}
-    mkdir ${project}
-    curl -L https://api.github.com/repos/redhat-cip/${project}/tarball | tar xz --directory ${project} --strip=1
-    popd
-    rm -rf ./src/${project}
-    mkdir ./src/${project}
-    cp /tmp/${project}/README* ./src/${project}/
-    cp -r /tmp/${project}/docs/ ./src/${project}/docs/ 2>/dev/null
-    rm -rf /tmp/${project}
+    curl -L https://api.github.com/repos/redhat-cip/${project}/tarball| tar xfz - --wildcards --no-anchored --strip-components=1 --one-top-level=./src/${project} '*.md'
+    find src -mindepth 3 -type d -not -name docs -exec rm -r '{}' \;
 done
+
+if [ -n "${DCI_ORIGINAL_PROJECT_NAME}" ]; then
+    test -d ./src/${DCI_ORIGINAL_PROJECT_NAME} && cp -rv ${DCI_ORIGINAL_PROJECT_SRC_DIR}/README*.md ./src/${DCI_ORIGINAL_PROJECT_NAME}
+fi
 
 npm install
 npm run build
