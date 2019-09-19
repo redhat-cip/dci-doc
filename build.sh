fcfcdb5625e35254356f9130aadac7a4afbc3b08
Guillaume Vincent <gvincent@redhat.com>
2019-09-19 09:28:12 +0200

    Refactor the build of the documentation
    Depends-On: I0181d9c6dcadfdc77fa38f6d2fad52604e2859c8
    Change-Id: I8181c0a1b45655628d4f6afd0cfabbd6ff59c404

--- build.sh
+++ build.sh
@@ -1,18 +1,12 @@
 #!/usr/bin/env bash
 
-projects="dci-ui python-dciclient python-dciauth dci-control-server dci-ansible dci-openstack-agent dci-dev-env dci-rhel-agent dci-downloader dci-infra ansible-playbook-dci-beaker"
+projects="dci-control-server dci-ui python-dciclient python-dciauth dci-downloader dci-openstack-agent dci-rhel-agent ansible-playbook-dci-beaker"
 for project in ${projects}
 do
-    pushd /tmp
-    rm -rf ${project}
-    mkdir ${project}
-    curl -L https://api.github.com/repos/redhat-cip/${project}/tarball | tar xz --directory ${project} --strip=1
-    popd
     rm -rf ./src/${project}
     mkdir ./src/${project}
-    cp /tmp/${project}/README* ./src/${project}/
-    cp -r /tmp/${project}/docs/ ./src/${project}/docs/ 2>/dev/null
-    rm -rf /tmp/${project}
+    cp ../${project}/README* ./src/${project}/
+    cp -r ../${project}/docs/ ./src/${project}/docs/ 2>/dev/null
 done
 
 npm install
