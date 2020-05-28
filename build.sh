4dcfc53f2638f0cf77b2253d9222a8a89fbc1ee3
Thomas Vassilian <tvassili@redhat.com>
2020-05-28 10:29:04 -0400

    Add dci-openshift-agent
    Change-Id: I4c1e2e18c61601df8be720a766dedd9d2e6fe0aa

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ temp_folder="/tmp/dci-doc"
 rm -rf ${temp_folder}
 mkdir ${temp_folder}
 cp -r . ${temp_folder}
-projects="dci-control-server dci-ui python-dciclient python-dciauth dci-downloader dci-openstack-agent dci-rhel-agent ansible-playbook-dci-beaker"
+projects="dci-control-server dci-ui python-dciclient python-dciauth dci-downloader dci-openstack-agent dci-rhel-agent ansible-playbook-dci-beaker dci-openshift-agent"
 for project in ${projects}
 do
     rm -rf ${temp_folder}/src/${project}
