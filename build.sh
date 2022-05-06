9280106a08e30bd6d9eadfdd75813380e5668a16
Frederic Lepied <flepied@redhat.com>
2022-05-06 18:01:44 +0200

    build the doc with dci-pipeline
    Change-Id: Icd5c46f41499c045114998ce921ac1f3e529e2a5
    Depends-On: I77fc711418d12130c19b727da97c6a495eec36a6

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ temp_folder="/tmp/dci-doc"
 rm -rf ${temp_folder}
 mkdir ${temp_folder}
 cp -r . ${temp_folder}
-projects="dci-control-server dci-ui python-dciclient python-dciauth dci-downloader dci-openstack-agent dci-rhel-agent dci-openshift-agent ansible-playbook-dci-beaker dci-openshift-app-agent"
+projects="dci-control-server dci-ui python-dciclient python-dciauth dci-downloader dci-openstack-agent dci-rhel-agent dci-openshift-agent ansible-playbook-dci-beaker dci-openshift-app-agent dci-pipeline"
 for project in ${projects}
 do
     echo ${project}
