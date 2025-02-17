3f1d489dbb764bc2fe03c996b235f79f21ab310f
Frederic Lepied <flepied@redhat.com>
2025-02-17 09:15:41 +0100

    add dci-ansible
    Change-Id: I0457d808ef6fbd69d0697d0986ce0040c729154b
    Depends-On: I24ed42e64f5791ec328507191ea3d4859fe56d0c
    Depends-On: Ic40e44db6f7fb6d5aea3e047b0948cf63edf1efa

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ rm -rf docs build
 mkdir build
 cp -ra src/* build
 
-projects="dci-control-server dci-ui python-dciclient python-dciauth dci-downloader dci-openstack-agent dci-rhel-agent dci-openshift-agent dci-openshift-app-agent dci-pipeline"
+projects="dci-control-server dci-ui python-dciclient python-dciauth dci-downloader dci-openstack-agent dci-rhel-agent dci-openshift-agent dci-openshift-app-agent dci-pipeline dci-ansible"
 for project in ${projects}; do
     echo "${project}"
-    rsync -avm --exclude=".*" --exclude="*node_modules/" --exclude="venv/*" --include="*/" --include="*.md"  --exclude="*" "../${project}/" "build/${project}/"
+    rsync -avm --include="*.md" --include="samples/*" --exclude=".*" --exclude="*node_modules/" --exclude="venv/*" --include="*/" --exclude="*" "../${project}/" "build/${project}/"
 done
 
 if [ ! -d venv ]; then
